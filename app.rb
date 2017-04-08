53da7c7d6950b5a58c49fe3184e98d9f5c453023
eaglesant <eagle_21@mail.ru>
2017-04-08 16:47:09 +0300

    Saved clients

--- app.rb
+++ app.rb
@@ -6,6 +6,12 @@ require 'sinatra/activerecord'
 
 set :database, "sqlite3:barbershop.db"
 
+before do
+	@barbers = Barber.all
+	@clients = Client.all
+
+end
+
 class Client < ActiveRecord::Base
 	
 end
@@ -15,6 +21,27 @@ class Barber < ActiveRecord::Base
 end
 
 get '/' do
-	@barbers = Barber.all
 	erb :index			
+end
+
+get '/visit' do
+  erb :visit
+end
+
+post '/visit' do
+	@user_name = params[:user_name]
+	@user_phone = params[:user_phone]
+	@date_time = params[:date_time]
+	@barber = params[:barber]
+	@color = params[:colorpicker]
+
+	c = Client.new :name => @user_name, :phone => @user_phone, :datestamp => @date_time, :barber => @barber, :color => @color
+
+  	c.save
+
+erb "<h2>Fank's you are enroied!!!</h2>"
+
+end
+get '/users' do
+  erb :users
 end
